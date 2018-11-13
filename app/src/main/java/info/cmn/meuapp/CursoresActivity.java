ddbe92566a6b46263c40faf57c83305d54ac2dc5
Manoel-Matias-bls <manoelmatias.cardoso@gmail.com>
2018-11-13 11:42:19 -0300

    Commit pós aula 07 Banco de Dados Cursores Finalizada

--- app/src/main/java/info/cmn/meuapp/CursoresActivity.java
+++ app/src/main/java/info/cmn/meuapp/CursoresActivity.java
@@ -1,6 +1,7 @@
 package info.cmn.meuapp;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
@@ -24,14 +25,74 @@ public class CursoresActivity extends AppCompatActivity {
     protected void onPostResume() {
         super.onPostResume();
 
+        deletarRegistros();
+
+    }
+
+    public void criarRegistros()
+    {
         DBHelper db = new DBHelper(getBaseContext());
         SQLiteDatabase banco = db.getWritableDatabase();
 
-        ContentValues ctv = new ContentValues();
-        ctv.put("nome", "Cliente 0");
-        ctv.put("email", "email@gmail.com");
+        ContentValues ctv;
 
-        banco.insert("clientes", null, ctv);
+        for (int i=1; i<21; i++)
+        {
+            ctv = new ContentValues();
+
+            ctv.put("nome", "Cliente Manoel"+Integer.toString(i));
+            ctv.put("email", "email"+Integer.toString(i)+"@gmail.com");
+
+            banco.insert("clientes", null, ctv);
+        }
+    }
+
+    public void editarRegistros()
+    {
+        DBHelper db = new DBHelper(getBaseContext());
+        SQLiteDatabase banco = db.getReadableDatabase();
+
+        Cursor cursor = banco.rawQuery("SELECT _id, nome, email FROM clientes", null);
+        ContentValues ctv;
+        if (cursor.moveToFirst())
+        {
+            //Tem registros
+            do{
+                ctv = new ContentValues();
+                ctv.put("nome", cursor.getString(cursor.getColumnIndex("nome"))+" ALTERADO" );
+
+                banco.update("clientes", ctv, "_id = " + cursor.getString(cursor.getColumnIndex("_id")), null);
+                Log.d("Cursor: ", cursor.getString(cursor.getColumnIndex("nome")));
+
+            }while (cursor.moveToNext());
+        }
+
+        cursor.close();
+    }
+
+    public void deletarRegistros()
+    {
+
+        DBHelper db = new DBHelper(getBaseContext());
+        SQLiteDatabase banco = db.getReadableDatabase();
+        //getWritableDatabase();
+        Cursor cursor = banco.rawQuery("SELECT _id, nome, email FROM clientes", null);
+        ContentValues ctv;
+        if (cursor.moveToFirst())
+        {
+            //Tem registros
+            do{
+                banco.delete("clientes", "_id = "+ cursor.getString(0), null);
+                Log.d("Cursor: ", cursor.getString(cursor.getColumnIndex("nome")));
+
+            }while (cursor.moveToNext());
+        }
+        else {
+            Log.d("Cursor", "SEM REGISTRO");
+        }
+
+        cursor.close();
+
     }
 
 }
